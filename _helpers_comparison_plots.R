dcf46e2d4a283ea09032289b8749e2447d696467
Alvaro <alvarobarbeira@gmail.com>
2017-12-06 13:45:49 -0600

    First plot

--- _helpers_comparison_plots.R
+++ _helpers_comparison_plots.R
@@ -3,16 +3,16 @@
 qq_plot_mt_vs_all_univariate <- function(mt_predixcan, predixcan, selected_model) {
   label_s_ <- paste0("PrediXcan, ", selected_model)
   
-  qq_mt_ <- qq_unif_plot_data_(mt_predixcan, label="MT-PrediXcan")
+  qq_mt_ <- qq_unif_plot_data_(mt_predixcan, label="CrossTissueXcan")
   qq_a_ <- qq_unif_plot_data_(predixcan, label="PrediXcan, All tissues")
   qq_s_ <- qq_unif_plot_data_(predixcan %>% filter(tissue == selected_model), label=label_s_)
   qq_ <- rbind(qq_a_, qq_s_, qq_mt_)
   
   config <- qq_plot_config_(
     colour_column="label",
-    colour_labels=c("MT-PrediXcan", "PrediXcan, All Tissues", textify(label_s_)),
-    colour_limits=c("MT-PrediXcan", "PrediXcan, All tissues", label_s_),
-    colours = c("black", "dodgerblue3", "firebrick3"),
+    colour_labels=c("CrossTissueXcan", "PrediXcan, All Tissues", textify(label_s_)),
+    colour_limits=c("CrossTissueXcan", "PrediXcan, All tissues", label_s_),
+    colours = c("#0072B2", "#009E73", "#D55E00"),
     colour_line = "black",
     point_size = 2,
     line_size =2
@@ -31,12 +31,13 @@ significant_discoveries_bar_plot <- function(mt_predixcan, predixcan, selected_m
     get_bonferroni_significant_gene_count(p_),
     get_bonferroni_significant_gene_count(mt_)
   )
-  x <- c( paste0(textify(selected_model)), 
-          paste0("All Tissues"),
-          paste0("MT-Predixcan"))
+  label_s_ <- paste0("PrediXcan,\n",textify(selected_model))
+  x <- c( label_s_, "PrediXcan,\nAll Tissues", "CrossTissueXcan")
   x <- factor(x, x)
   d <- data.frame(x=x, y=y)
   
   ggplot() + paper_plot_theme_a() + geom_bar(data = d, mapping=aes(x=x, y=y, fill=x), stat="identity", show.legend=F) +
-    xlab("Method") + ylab(NULL)
+    xlab("Method") + ylab(NULL) + 
+    scale_fill_manual(values=c("#0072B2", "#009E73", "#D55E00"),
+                      limits=c("CrossTissueXcan", "PrediXcan,\nAll Tissues", label_s_))
 }
--- paper_material.py
+++ paper_material.py
@@ -26,15 +26,43 @@ def _advance_cursor(c, x, y):
     return (c[0]+x, c[1]+y)
 
 
-def _kot(dwg, _c, text, ox=-40):
-    dwg.add(dwg.text(text, insert=(_c[0]+ox, _c[1]+50), fill='black', style="font-size:40;font-family:Arial;font-weight:bold;stroke:black;stroke-width:1;fill:black"))
+def _kot(dwg, _c, text, ox=-40, oy=50):
+    dwg.add(dwg.text(text, insert=(_c[0]+ox, _c[1]+oy), fill='black', style="font-size:40;font-family:Arial;font-weight:bold;stroke:black;stroke-width:1;fill:black"))
 
+def figure_1(args):
+    _p = "_fig1.svg"#os.path.join(args.output_folder, "fig1.svg")
+
+    # diagram is (341,972); plots are 600,600
+    _1_size = (341, 972)
+    _2_size = (470, 470)
+    _size = (_1_size[0]+130+_2_size[0], 980)
+
+    dwg = svgwrite.Drawing(_p, size=_size)
+    dwg.add(dwg.rect(insert=(0, 0), size=_size, fill="rgb(255,255,255)"))
+
+    _c = (40,0) # conceptual cursor
+    dwg.add(dwg.image(os.path.join(args.input_folder_2, "S-Predixcan-MT-diagram.png"), _c, _1_size))
+    _kot(dwg, _c, "a", ox=-30, oy=30)
+
+    _c = _advance_cursor(_c, _1_size[0]+90,0)
+    dwg.add(dwg.image(os.path.join(args.plots_folder, "ukb", "UKB_Cholesterol_qq.png"), _c, _2_size))
+    _kot(dwg, _c, "b", ox=-20, oy=30)
+
+    _c =_advance_cursor (_c, 0, _2_size[1]+40)
+    dwg.add(dwg.image(os.path.join(args.plots_folder, "ukb", "UKB_Cholesterol_significant_bars.png"), _c, _2_size))
+    _kot(dwg, _c, "c", ox=-20, oy=30)
+
+    dwg.save()
+    t = os.path.join(args.output_folder, "fig-multi-tissue-better-than-predixcan.png")
+    to_png(_p, t)
+    os.remove(_p)
 
 ########################################################################################################################
 def run(args):
     if not os.path.exists(args.output_folder):
         os.makedirs(args.output_folder)
 
+    figure_1(args)
 
 if __name__ == "__main__":
     class Dummy(object):
